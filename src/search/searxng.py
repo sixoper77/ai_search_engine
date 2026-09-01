9b18145588f1cc4a23f91da90b96d64866a574a6
Andrey <vstup230921@gmail.com>
2026-09-01 22:02:49 +0300

    refactor: SearxngSearch naming and params extraction

--- src/search/searxng.py
+++ src/search/searxng.py
@@ -10,10 +10,10 @@ class SearxngSearch(BaseSearch):
     searxng_url = settings.searxng_url
 
     @classmethod
-    async def search(cls, query: list[str], photo_query: str, language: str) -> dict:
+    async def search(cls, queries: list[str], photo_query: str, language: str) -> dict:
         async with asyncio.TaskGroup() as tg:
-            url_tasks = [tg.create_task(cls.search_urls(q, language)) for q in query]
-            task_photo = tg.create_task(cls.search_photo(photo_query, language))
+            url_tasks = [tg.create_task(cls.search_urls(q, language)) for q in queries]
+            task_photo = tg.create_task(cls.search_photos(photo_query, language))
         url_results = [task.result() for task in url_tasks]
         return url_results, task_photo.result()
 
@@ -29,14 +29,12 @@ class SearxngSearch(BaseSearch):
         return result
 
     @classmethod
-    async def search_photo(cls, query: str, language: str) -> dict:
-        result = await session.search(
-            cls.searxng_url,
-            params={
-                "q": query,
-                "language": language,
-                "format": "json",
-                "categories": "images",
-            },
-        )
+    async def search_photos(cls, query: str, language: str) -> dict:
+        params = {
+            "q": query,
+            "language": language,
+            "format": "json",
+            "categories": "images",
+        }
+        result = await session.search(cls.searxng_url, params=params)
         return result
